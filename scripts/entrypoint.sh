2f7d591c8160e321b443407e4b631799cc320c8b
Roman Shelekhov <shelekhoff@gmail.com>
2026-02-09 21:33:37 +0400

    write .env in container for prisma migrate deploy

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+echo "DATABASE_URL=${DATABASE_URL}" > .env
+
 echo "Running database migrations..."
 bunx --bun prisma migrate deploy --schema src/infrastructure/prisma/schema.prisma
 
